04e28bd24ce6ec3457411ec33af9ad8039a4633d
RafaelRossales <rapha2704@gmail.com>
2021-02-09 22:19:32 -0300

    Implementing Admin pages

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,7 +6,10 @@ import App from './App'
 import store from './config/store'
 import router from './config/router'
 import './config/bootstrap'
+import './config/msg'
 
+//Temporario
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFuYSIsImVtYWlsIjoiYW5hQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2MTI4MzAxNjIsImV4cCI6MTYxMzA4OTM2Mn0.Fjtmkj9AuQHXjoMP40Eiqlodi-U1Zm354dGgtQb-xIQ'
 Vue.config.productionTip = false
 
 new Vue({
